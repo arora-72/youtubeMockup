744e41e69c78f8de3e291d6348e75036469da5f9
arora_72 <indresh.arora2016@vitstudent.ac.in>
2017-06-17 23:12:59 +0530

    Initial Commit

---
+++ youtube mockup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youtube mockup
+//
+//  Created by arora_72 on 17/06/17.
+//  Copyright © 2017 indresh arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
